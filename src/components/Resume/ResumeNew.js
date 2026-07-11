d970cbc244a9e1890f954e2c72f161acd9d47958
hsminh <hosyminh11820004@email.com>
2026-07-11 19:05:30 +0700

    feat: switch resume source to FinalCV.pdf

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/resume.pdf";
+import pdf from "../../Assets/FinalCV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import { useI18n } from "../../i18n/I18nContext";
